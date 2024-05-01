2c25525ffde3611b254e133d26ea21466f3d444c
rikuto11 <rkthntn@gmail.com>
2024-05-01 15:53:13 +0900

    Add 保存処理

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,4 +21,10 @@ public function create()
 {
     return view('posts/create');
 }
+public function store(Request $request, Post $post)
+{
+$input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+}
 }
